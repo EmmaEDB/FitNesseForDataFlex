00a89a9281e8c0356e72815044ea83750d4ebe2b
Ola Eldøy <ola@eldoy.no>
2016-06-03 22:41:54 +0200

    StartFitnesse.bat will instruct to download fitnesse-standalone.jar if necessary.

--- FitNesse/StartFitnesse.bat
+++ FitNesse/StartFitnesse.bat
@@ -1,2 +1,8 @@
-java -jar fitnesse-standalone.jar -p 8081
+@if exist fitnesse-standalone.jar (
+    java -jar fitnesse-standalone.jar -p 8081
+
+) else (
+    @echo download fitnesse-standalone.jar from http://fitnesse.org/
+)
+@echo ...
 @pause
